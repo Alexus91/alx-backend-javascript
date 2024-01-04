569d0b2e7c696e1d55d08283b8957cce0a68a188
Alexus91 <mrfadlkamal@gmail.com>
2024-01-04 00:25:56 +0000

    fixing the promises are all settled it should return an array with the structure

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,6 +2,12 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => (
+      results.map((out) => ({
+        status: out.status,
+        value: out.status === 'fulfilled' ? out.value : String(out.reason),
+      }))
+    ));
 }
 export default handleProfileSignup;
