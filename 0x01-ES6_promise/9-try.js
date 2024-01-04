9dee1ef967f9d7a8824c6303ef232d332c568d91
Alexus91 <mrfadlkamal@gmail.com>
2024-01-04 00:57:25 +0000

    10. Await / Async

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,16 @@
+function guardrail(mathFunction) {
+  const queue = [];
+  let value;
+
+  try {
+    value = mathFunction();
+  } catch (err) {
+    value = err.toString();
+  }
+
+  queue.push(value);
+  queue.push('Guardrail was processed');
+
+  return queue;
+}
+export default guardrail();
