d9f6bc98bc119f755f7da264b24cfb352ec1c76d
Alexus91 <mrfadlkamal@gmail.com>
2024-01-04 19:11:06 +0000

    8. Primitive - Holberton Class

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,36 @@
+export default class HolbertonClass {
+  /**
+   * @param {number} size - the number of the students in the clas
+   * @param {string} location -  class location
+   */
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    this._size = value;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(value) {
+    this._location = value;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this.size;
+    }
+    if (hint === 'string') {
+      return this.location;
+    }
+    return this;
+  }
+}
