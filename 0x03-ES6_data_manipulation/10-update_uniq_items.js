d204b07d1cefef1fe40e3df6bc33f267c93862ec
Alexus91 <mrfadlkamal@gmail.com>
2024-01-10 02:11:41 +0000

    11. Weak link data structure

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,12 @@
+function updateUniqueItems(groceriesMap) {
+  if (!(groceriesMap instanceof Map)) {
+    throw new Error("Cannot process");
+  }
+
+  for (const [item, quantity] of groceriesMap.entries()) {
+    if (quantity === 1) {
+      groceriesMap.set(item, 100);
+    }
+  }
+}
+export default updateUniqueItems;
---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,15 @@
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 1);
+  } else {
+    const count = weakMap.get(endpoint);
+    weakMap.set(endpoint, count + 1);
+
+    if (count + 1 >= 5) {
+      throw new Error('Endpoint load is high');
+    }
+  }
+}
+
