af5b232416fb61e0ab932778de5e3bb8c9f4444e
Alexus91 <mrfadlkamal@gmail.com>
2024-01-10 01:27:58 +0000

    4. Combine

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,19 @@
+function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const updatedGrade = newGrades.find((grade) => grade.studentId === student.id);
+
+      if (updatedGrade) {
+        return {
+          ...student,
+          grade: updatedGrade.grade,
+        };
+      }
+      return {
+        ...student,
+        grade: 'N/A',
+      };
+    });
+}
+export default updateStudentGradeByCity;
