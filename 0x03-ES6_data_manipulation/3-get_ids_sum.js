21fc4b2a6d295a0019b2189ba26591e88731a88e
Alexus91 <mrfadlkamal@gmail.com>
2024-01-10 01:16:57 +0000

    3. Reduce &&  2. Filter

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,10 @@
+function getStudentIdsSum(students) {
+  if (students instanceof Array) {
+    return students.reduce(
+      (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
+      0,
+    );
+  }
+  return 0;
+}
+export default getStudentIdsSum;
