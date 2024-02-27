35147c134abc90f5e43ecbd58e8675715892904d
Alexus91 <mrfadlkamal@gmail.com>
2024-02-27 19:13:30 +0100

    last try to fix requ

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,14 +1,33 @@
 const express = require('express');
 
+/**
+ * Creates an HTTP server using Express module.
+ * @module
+ */
 const app = express();
-const port = 1245;
 
+const PORT = 1245;
+
+/**
+ * Sends "Hello Holberton School!" response for the root endpoint.
+ * @name GET /
+ * @function
+ * @memberof module:4-http-express
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @returns {undefined}
+ */
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.listen(port, () => {
-  //   console.log(`Example app listening at http://localhost:${PORT}`);
+/**
+ * The HTTP server listening on port 1245.
+ * @type {Object}
+ * @memberof module:4-http-express
+ */
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
 });
 
 module.exports = app;
