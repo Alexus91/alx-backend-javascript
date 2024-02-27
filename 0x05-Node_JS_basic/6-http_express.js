cc3513ccbc33bb173929315d9fdd5f2709b270fd
Alexus91 <mrfadlkamal@gmail.com>
2024-02-27 19:06:48 +0100

    6. Create a small HTTP server using Express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 1245;
+
+/**
+ * Sends "Hello Holberton School!" response.
+ * @name GET /
+ * @function
+ * @memberof module:4-http-express
+ * @param {Object} req - request object
+ * @param {Object} res - response object
+ * @returns {undefined}
+ */
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+/**
+ * The HTTP server listening on port 1245.
+ * @type {Object}
+ * @memberof module:4-http-express
+ */
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
+module.exports = app;
