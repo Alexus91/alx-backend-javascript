b09bfaf524dd431b6209d0f9ed9a04a1ff6f331b
Alexus91 <mrfadlkamal@gmail.com>
2024-02-29 14:52:26 +0100

    6. Async tests with done

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,10 @@
+function getPaymentTokenFromAPI(success) {
+  // eslint-
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ data: 'Successful response from the API' });
+    }
+  });
+}
+
+module.exports = getPaymentTokenFromAPI;
